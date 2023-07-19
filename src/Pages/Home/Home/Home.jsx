cd7a4775aab40ed1c28c2e07cd18f476f067224a
ismailJihad29 <ismailhossenjihad234@gmail.com>
2023-07-19 20:50:51 +0600

    single toy details done

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Gallery from '../Gallery/Gallery';
 import CategoryShop from '../CategoryShop/CategoryShop';
+import Testimonials from '../Testimonials/Testimonials';
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <Gallery />
-            <CategoryShop/>
+            <CategoryShop />
+            <Testimonials/>
         </div>
     );
 };
--- src/Pages/ToysDetails/ToysDetails.jsx
+++ src/Pages/ToysDetails/ToysDetails.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 import Rating from "react-rating";
+import { BiSolidCategory,  } from "react-icons/bi"
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ToysDetails = () => {
   const data = useLoaderData();
+  const toaster = () => toast("Successfully added to your cart!!");
 
   const {
-    _id,
     name,
     price,
     pictureURL,
@@ -14,30 +17,48 @@ const ToysDetails = () => {
     description,
     sellerEmail,
     sellerName,
-    availableQuantity,
+      availableQuantity,
+      subCategory
   } = data;
   return (
-    <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:flex-row">
-        <img src={pictureURL} className="max-w-sm rounded-lg shadow-2xl" />
-        <div className="ml-5">
-          <h1 className="text-5xl font-bold font-primary uppercase">{name}</h1>
-          <h1 className="text-xl font-semibold"> <span className="text-pink-500">Seller:</span> {sellerName}</h1>
-          <h1 className="text-xl font-semibold"><span className="text-pink-500">Email:</span> {sellerEmail}</h1>
-                  <p className="font-semibold mb-3">
-                      
-            {" "}
-            <span className="text-pink-500">Available Quantity:</span> {availableQuantity}
-          </p>
-          <p>
-            <Rating initialRating={rating} readonly />
-                  </p>
-                  <p className="text-2xl font-bold font-sans text-purple-400 uppercase  animate-pulse">
-                      Price: { price}
-                  </p>
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row">
+          <img src={pictureURL} className="max-w-sm rounded-lg shadow-2xl" />
+          <div className="ml-5">
+            <div className="badge badge-warning gap-2">
+             <BiSolidCategory/>
+              {subCategory}
+            </div>
 
-          <p className="py-6">{description}</p>
-          <button className="btn button-primary">Buy Now</button>
+            <h1 className="text-5xl font-bold font-primary uppercase">
+              {name}
+            </h1>
+            <h1 className="text-xl font-semibold">
+              {" "}
+              <span className="text-pink-500">Seller:</span> {sellerName}
+            </h1>
+            <h1 className="text-xl font-semibold">
+              <span className="text-pink-500">Email:</span> {sellerEmail}
+            </h1>
+            <p className="font-semibold mb-3">
+              {" "}
+              <span className="text-pink-500">Available Quantity:</span>{" "}
+              {availableQuantity}
+            </p>
+            <p>
+              <Rating initialRating={rating} readonly />
+            </p>
+            <p className="text-2xl font-bold font-sans text-purple-400 uppercase  animate-pulse">
+              Price: {price}
+            </p>
+
+            <p className="py-6">{description}</p>
+            <button onClick={toaster} className="btn button-primary">
+              Buy Now
+            </button>
+          </div>
+          <ToastContainer />
         </div>
       </div>
     </div>
