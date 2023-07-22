7e3f34f9338a700f0839193a5de9895874ee1649
ismailJihad29 <ismailhossenjihad234@gmail.com>
2023-07-22 17:35:48 +0600

    blog2

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -19,7 +19,64 @@ const Blog = () => {
         </div>
       </div>
       {/* qus 2 */}
-     
+      {/* <div className="card w-full bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">What is an refresh token?.</h2>
+          <p>
+            A <span className="text-rose-500"> refresh token </span>
+            token is like a master key that helps you get a new access key when
+            the old one expires. So, instead of logging in again, you can use
+            this master key to get a fresh access key without any hassle. The
+            master key lasts longer and is more reliable than the access key.
+          </p>
+        </div>
+      </div> */}
+      {/* qus 3 */}
+      <div className="card w-full bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title"> Comparing SQL and NoSQL databases ??</h2>
+          <p>
+            <span className="text-rose-500"> SQL</span> databases are like
+            organized filing cabinets with tables, rows, and columns. They use a
+            structured language (SQL) to manage and retrieve data. Good for
+            complex queries and ensuring data consistency, but less flexible for
+            changing data models.
+          </p>
+          <p>
+            <span className="text-rose-500">NoSQL</span> databases are like big
+            bags where you can throw anything in. They use various data models
+            like key-value, document, or graph to store data. Great for handling
+            large amounts of unstructured or diverse data, but may not provide
+            the same level of consistency as SQL databases
+          </p>
+        </div>
+      </div>
+      {/* qus 4 */}
+      <div className="card w-full bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">What is express js?.</h2>
+          <p>
+            <span className="text-rose-500"> Express.js </span>
+            is a tool that helps developers build web applications and APIs
+            using JavaScript. It makes creating web services easier and faster.
+          </p>
+        </div>
+      </div>
+      {/*   qus 5 */}
+      <div className="card w-full bg-base-100 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">What is Nest JS??</h2>
+          <p>
+            In MongoDB, <span className="text-rose-500">"aggregate"</span>
+            is a special function that lets you do advanced operations on your
+            data. You can group, filter, and calculate things from your
+            database. It works in stages, where each stage does a specific task,
+            and the data flows through these stages to get the final result.
+            It's like a powerful tool for analyzing and summarizing data in
+            MongoDB.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
